bfe0e0d30151bdc8450493b670154388451d49ef
rabsztok <rabsztok@gmail.com>
2013-11-24 23:55:54 +0100

    Scaffoldy, download plików, można generować już dane dla riaka

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,8 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (http://rvm.io)
-
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
+require 'mina/rbenv'
+require 'mina_sidekiq/tasks'
 
 set :term_mode, :system
 set :domain, 'merch-home-riak'
@@ -41,7 +35,7 @@ end
 desc "Deploys the current version to the server."
 task :deploy => :environment do
   deploy do
-    # Put things that will set up an empty directory into a fully set-up instance of your project.
+    invoke :'sidekiq:quiet'
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
@@ -50,14 +44,7 @@ task :deploy => :environment do
 
     to :launch do
       queue 'sudo /etc/init.d/apache2 restart'
+      invoke :'sidekiq:restart'
     end
   end
 end
-
-# For help in making your deploy script, see the Mina documentation:
-#
-#  - http://nadarei.co/mina
-#  - http://nadarei.co/mina/tasks
-#  - http://nadarei.co/mina/settings
-#  - http://nadarei.co/mina/helpers
-
